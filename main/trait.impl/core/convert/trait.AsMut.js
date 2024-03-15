835969c1e01ea95b098d2386b38c246f2efeca6e
kl-botsu <botsu@khonsulabs.com>
2024-03-15 14:39:31 +0000

    Deploying to gh-pages from @ khonsulabs/musegc@335d8c66837e09d1d0b110ac185a39e0836d907f 🚀

---
+++ main/trait.impl/core/convert/trait.AsMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"musegc":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;<a class=\"struct\" href=\"musegc/struct.CollectionGuard.html\" title=\"struct musegc::CollectionGuard\">CollectionGuard</a>&gt; for <a class=\"struct\" href=\"musegc/struct.CollectionGuard.html\" title=\"struct musegc::CollectionGuard\">CollectionGuard</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
