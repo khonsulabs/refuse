5790e17dea57a6b387ff36ba7b62a8777d2c40de
kl-botsu <botsu@khonsulabs.com>
2024-03-24 13:59:33 +0000

    Deploying to gh-pages from @ khonsulabs/refuse@43f7f5e63f36d7d09b80d5cee8ac5ef2467f35e8 🚀

---
+++ main/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"refuse":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"refuse/struct.WouldDeadlock.html\" title=\"struct refuse::WouldDeadlock\">WouldDeadlock</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
