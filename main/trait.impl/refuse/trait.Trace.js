9f6c7b21a6f675549ae17686ae084923f1169bbc
kl-botsu <botsu@khonsulabs.com>
2024-08-02 21:50:17 +0000

    Deploying to gh-pages from @ khonsulabs/refuse@c8667400c8e3435e6ed0f3819ef777ef82cb6ffe 🚀

--- main/trait.impl/refuse/trait.Trace.js
+++ main/trait.impl/refuse/trait.Trace.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "refuse":[],
-"refuse_pool":[["impl <a class=\"trait\" href=\"refuse/trait.Trace.html\" title=\"trait refuse::Trace\">Trace</a> for <a class=\"struct\" href=\"refuse_pool/struct.RefString.html\" title=\"struct refuse_pool::RefString\">RefString</a>"],["impl <a class=\"trait\" href=\"refuse/trait.Trace.html\" title=\"trait refuse::Trace\">Trace</a> for <a class=\"struct\" href=\"refuse_pool/struct.RootString.html\" title=\"struct refuse_pool::RootString\">RootString</a>"]]
+"refuse_pool":[["impl Trace for <a class=\"struct\" href=\"refuse_pool/struct.RefString.html\" title=\"struct refuse_pool::RefString\">RefString</a>"],["impl Trace for <a class=\"struct\" href=\"refuse_pool/struct.RootString.html\" title=\"struct refuse_pool::RootString\">RootString</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
